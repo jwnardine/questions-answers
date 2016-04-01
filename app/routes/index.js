77286add3218a28337e01be6da0726d9aff886e7
Jon Nardine <jwnardine@gmail.com>
2016-04-01 09:14:05 -0700

    change questionsAuthor to questionAuthor

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,21 +3,21 @@ import Ember from 'ember';
   var questions = [{
   id: 1,
   questionTitle: "Why are dogs and cats enemies?",
-  questionsAuthor: "Ace Ventura",
+  questionAuthor: "Ace Ventura",
   questionContent: "I was just wondering because my dog and cat hate eachother.",
-  answer: "Answer"
+  answer: "This question has not yet been answered."
 }, {
   id: 2,
   questionTitle: "Which came first, the chicken or the egg?",
-  questionsAuthor: "Ronald McDonald",
+  questionAuthor: "Ronald McDonald",
   questionContent: "I need to know because I raise chickens and this seems important.",
-  answer: "Answer"
+  answer: "This question has not yet been answered."
 }, {
   id: 3,
   questionTitle: "Who shot JR?",
-  questionsAuthor: "Patrick Duffy",
+  questionAuthor: "Patrick Duffy",
   questionContent: "I totally missed the season premiere because my raquetball tournament was on the same night.",
-  answer: "Answer"
+  answer: "This question has not yet been answered."
 }];
   export default Ember.Route.extend({
     model() {
