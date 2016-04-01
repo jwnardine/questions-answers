11a53ee40b15b43cd24485d5dfec2d048ac20499
Jon Nardine <jwnardine@gmail.com>
2016-04-01 09:53:40 -0700

    add question content to question-tile

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
+  <li>Question: {{question.questionTitle}}</li>
+  <li>Submitted by: {{question.questionAuthor}}</li>
+  <li>Content: {{question.questionContent}}</li>
+  <li>Answer: {{question.answer}}</li>
+  <hr>
+  
 export default Ember.Component.extend({
+
 });
