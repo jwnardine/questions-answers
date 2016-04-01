2efac7913ba80ef02501afce82e08d0ddf1dea6f
Jon Nardine <jwnardine@gmail.com>
2016-04-01 09:06:02 -0700

    add questions template to index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+  var questions = [{
+  id: 1,
+  questionTitle: "Why are dogs and cats enemies?",
+  questionsAuthor: "Ace Ventura",
+  questionContent: "I was just wondering because my dog and cat hate eachother.",
+  answer: "Answer"
+}, {
+  id: 2,
+  questionTitle: "Which came first, the chicken or the egg?",
+  questionsAuthor: "Ronald McDonald",
+  questionContent: "I need to know because I raise chickens and this seems important.",
+  answer: "Answer"
+}, {
+  id: 3,
+  questionTitle: "Who shot JR?",
+  questionsAuthor: "Patrick Duffy",
+  questionContent: "I totally missed the season premiere because my raquetball tournament was on the same night.",
+  answer: "Answer"
+}];
+  export default Ember.Route.extend({
+    model() {
+      return questions;
+    },
 });
