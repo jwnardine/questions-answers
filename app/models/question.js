1e26cc9305aa17ee1a638d0c6356c2ee437a7ad0
Jon Nardine <jwnardine@gmail.com>
2016-04-01 09:19:16 -0700

    add content to question model

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  questionTitle: DS.attr(),
+  questionAuthor: DS.attr(),
+  questionContent: DS.attr(),
+  answer: DS.attr()
 });
