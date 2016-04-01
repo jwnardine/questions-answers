60ebad6798c2060813a3fefce524ce50f69c6adb
Jon Nardine <jwnardine@gmail.com>
2016-04-01 11:50:01 -0700

    add functionality so user can update a current question

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,17 @@ import Ember from 'ember';
         this.transtionTo('index');
       },
 
+      update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+
+
       destroyQuestion(question) {
         question.destroyRecord();
         this.transitionTo('index');
