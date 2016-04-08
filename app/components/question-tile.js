dd69583a4a94ea51cb13cd3c3715216af0a545c7
Jon Nardine <jwnardine@gmail.com>
2016-04-08 15:11:20 -0700

    add favorite questions service

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
 
   computedContentAuthor: Ember.computed('question.questionTitle', 'question.questionAuthor', function() {
     return this.get('question.questionTitle') + ' || By: ' + this.get('question.questionAuthor');
   }),
 
   actions: {
+    addToFavorites(favorite) {
+      this.get('favoriteQuestions').add(favorite);
+    },
+
     update(question, params) {
       this.sendAction('update', question, params);
     }
